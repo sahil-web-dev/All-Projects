67b7e21d4603f7f9929cf555811f038f0a0a4d3f
sahil-web-dev <sahil123vid@gmail.com>
2020-08-12 15:58:31 +0530

    Create alphabets.java

---
+++ Java Programs/alphabets.java
@@ -0,0 +1,12 @@
+/* JAVA PROGRAM TO PRINT ALPHABETS FROM A TO Z */
+public class alphabets
+{
+	public static void main(String[] args)
+	{
+		for(char c='a';c<='z';c++)
+		{
+			System.out.print("\t"+c);
+		}
+	}
+	
+}
